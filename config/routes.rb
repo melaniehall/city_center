f22aed44919cf3eab69e926708a79a83a9ef2362
melaniehall <melaniehalldesign@gmail.com>
2013-06-17 12:52:27 -0500

    Added Devise and Initial Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 CityCenter::Application.routes.draw do
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
