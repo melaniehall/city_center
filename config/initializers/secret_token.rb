c8d044eeab551326450a4fe636d8ee872c6741c0
melaniehall <melaniehalldesign@gmail.com>
2013-06-26 10:50:58 -0500

    Added ruby 2.0 to Gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CityCenter::Application.config.secret_token = 'fcf52716cd46eeb2d527aef236fbb5942375786b3467787452c23a36149f690618caf9ec82b278a929bd136d2565896eb7b954b98a55e483b09bc37aca8b9f91'
+# if Rails.env.development? or Rails.env.test?
+  CityCenter::Application.config.secret_token = 'fcf52716cd46eeb2d527aef236fbb5942375786b3467787452c23a36149f690618caf9ec82b278a929bd136d2565896eb7b954b98a55e483b09bc37aca8b9f91'
+# else
+#   CityCenter::Application.config.secret_token = ENV['SECRET_TOKEN']
+# end
